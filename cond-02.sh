ddeda1b1d45e8693f381d8a988f31b4c17b9edf7
ArpanaM <muriki.arpana@gmail.com>
2025-02-13 21:12:01 +0530

    if conditions

--- cond-02.sh
+++ cond-02.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 read -p "enter your age" age
-if [ ! -z "${age}" -a "${age}" -lt 18 ]; then
+if[ ! -z "${age}" -a "${age}" -lt 18 ]; then
   echo you are minor
-  elif [ ! -z "${age}" -a "${age}" -gt 80 ]; then
+  elif[ ! -z "${age}" -a "${age}" -gt 80 ]; then
     echo you are senior citizen
     else
       echo you are major
