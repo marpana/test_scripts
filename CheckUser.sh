8617779ec6533f8a7ee400c9e986f2e215183fe3
ArpanaM <muriki.arpana@gmail.com>
2025-02-14 15:39:13 +0530

    if conditions

--- CheckUser.sh
+++ CheckUser.sh
@@ -2,7 +2,7 @@
 USER_UID=$(id -u)
 echo USER_UID
 if [ ${USER_UID} -ne 0 ]; then
-  echo -e "\e[31m You must be a root user to perform this script \e[0m"
+  echo -e "\e[31mYou must be a root user to perform this script \e[0m"
   exit
   fi
   yum install nginx -y
