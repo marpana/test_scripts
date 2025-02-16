02513b8fef53795c7fea64db8c5f9faca4a6ea47
ArpanaM <muriki.arpana@gmail.com>
2025-02-16 16:51:31 +0530

    Catalogue and Frontend script updated

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 echo catalogue setup................
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+dnf install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-echo frontend setup............
+#set -e
 dnf install nginx -y
+echo frontend setup............
+if [ $? -ne 0 ]; then
+  echo "Nginx install Failed.."
+  exit
+fi
+
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
+
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+if [ $? -ne 0 ]; then
+  echo "Download Frontend Failed.."
+  exit
+fi
+
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
