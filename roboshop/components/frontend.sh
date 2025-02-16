16c11a37e0101d10d52f269bb72cdd96100bdd3c
ArpanaM <muriki.arpana@gmail.com>
2025-02-16 12:46:04 +0530

    frontend script done

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo frontend setup............
-yum install nginx -y
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
+dnf install nginx -y
 systemctl enable nginx
 systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 
+systemctl restart nginx
