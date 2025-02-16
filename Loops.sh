f89e3b79e76ad9fd7789decc19cf31467129bfc9
ArpanaM <muriki.arpana@gmail.com>
2025-02-16 05:42:32 +0530

    Loops

--- Loops.sh
+++ Loops.sh
@@ -7,6 +7,7 @@ do
 done
 echo "This is outside the for loop"
 
+echo "**************While loop*************"
 myval=1
 while [ $myval -le 10 ]
 do
@@ -14,3 +15,14 @@ do
   myval=$(($myval + 1))
   sleep 0.5
   done
+echo "This is outside the while loop"
+
+echo "******************Until***************"
+number=1
+until [ $number -ge 11 ]
+do
+  echo "$number"
+  number=$((number+1))
+  done
+  echo "This is Outside until loop"
+
